03e840d58ba175417c3fc7d0f6b48be96283d2ee
BeaComp <102777924+BeaComp@users.noreply.github.com>
2023-05-13 12:42:09 -0300

    Project create

---
+++ dsbooks/src/main/java/com/devbooks/dsbooks/DsbooksApplication.java
@@ -0,0 +1,13 @@
+package com.devbooks.dsbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsbooksApplication.class, args);
+	}
+
+}
---
+++ dsbooks/src/test/java/com/devbooks/dsbooks/DsbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbooks.dsbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
