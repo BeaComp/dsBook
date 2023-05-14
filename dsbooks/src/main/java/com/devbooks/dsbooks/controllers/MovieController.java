17d4d03a9852db8a739e081618111c281b44690d
BeaComp <102777924+BeaComp@users.noreply.github.com>
2023-05-14 00:37:55 -0300

    Get game by id

--- dsbooks/src/main/java/com/devbooks/dsbooks/controllers/MovieController.java
+++ dsbooks/src/main/java/com/devbooks/dsbooks/controllers/MovieController.java
@@ -3,9 +3,11 @@ package com.devbooks.dsbooks.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devbooks.dsbooks.dto.MovieDTO;
 import com.devbooks.dsbooks.dto.MovieMinDTO;
 import com.devbooks.dsbooks.entities.Movie;
 import com.devbooks.dsbooks.services.MovieService;
@@ -16,6 +18,14 @@ public class MovieController {
     
     @Autowired
     private MovieService movieService;
+
+
+    @GetMapping(value = "/{id}")
+    public MovieDTO findById(@PathVariable Long id) {
+        MovieDTO result = movieService.findById(id);
+        return result;
+    }
+
 
     @GetMapping
     public List<MovieMinDTO> findAll() {
--- dsbooks/src/main/java/com/devbooks/dsbooks/services/MovieService.java
+++ dsbooks/src/main/java/com/devbooks/dsbooks/services/MovieService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devbooks.dsbooks.dto.MovieDTO;
 import com.devbooks.dsbooks.dto.MovieMinDTO;
 import com.devbooks.dsbooks.entities.Movie;
 import com.devbooks.dsbooks.repositories.MovieRespository;
@@ -13,7 +15,15 @@ public class MovieService {
 
     @Autowired
     private MovieRespository movieRepository;
+
+    @Transactional(readOnly = true)
+    public MovieDTO findById(Long id) {
+        Movie result = movieRepository.findById(id).get();
+        MovieDTO dto = new MovieDTO(result);
+        return dto;
+    }
     
+    @Transactional(readOnly = true)
     public List<MovieMinDTO> findAll() {
         List<Movie> result = movieRepository.findAll();
         return result.stream().map(x -> new MovieMinDTO(x)).toList();
